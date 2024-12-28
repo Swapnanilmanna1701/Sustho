f618c36dc1ff66162efda08668cf3f54544e6818
Swapnanilmanna1701 <sm.22u10337@btech.nitdgp.ac.in>
2024-12-28 23:02:59 +0530

    first commit

---
+++ src/utils/constants/fonts.ts
@@ -0,0 +1,9 @@
+import { Inter, DM_Sans } from "next/font/google";
+import localFont from "next/font/local";
+
+
+
+export const inter = Inter({
+    subsets: ["latin"],
+    variable: "--font-inter",
+});
