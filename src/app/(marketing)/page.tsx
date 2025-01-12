a66b35cd1021ed7a1f3ce9f22fd6d9b27e5a78f6
Swapnanilmanna1701 <sm.22u10337@btech.nitdgp.ac.in>
2025-01-12 23:10:58 +0530

    button customization

--- src/app/(marketing)/page.tsx
+++ src/app/(marketing)/page.tsx
@@ -25,6 +25,7 @@ import State from "@/components/state";
 import PopImagesParagraph from "@/components/preview";
 import AnimatedLogoCloud from "@/components/logo-cloud";
 import TextRevealButton from "@/components/text-reveal-button";
+import StarWarsButton from "@/components/star-button";
 
 //import { Spotlight } from "@/components/ui/spotlight";
 
@@ -36,16 +37,8 @@ const HomePage = async () => {
       <MaxWidthWrapper>
         <div className="flex flex-col items-center justify-center w-full text-center bg-gradient-to-t from-background">
           <AnimationContainer className="flex flex-col bg-black items-center justify-center w-full text-center">
-            <button className="group relative grid overflow-hidden rounded-full px-4 py-1 shadow-[0_1000px_0_0_hsl(0_0%_20%)_inset] transition-colors duration-200">
-              <span>
-                <span className="spark mask-gradient absolute inset-0 h-[100%] w-[100%] animate-flip overflow-hidden rounded-full [mask:linear-gradient(white,_transparent_50%)] before:absolute before:aspect-square before:w-[200%] before:rotate-[-90deg] before:animate-rotate before:bg-[conic-gradient(from_0deg,transparent_0_340deg,white_360deg)] before:content-[''] before:[inset:0_auto_auto_50%] before:[translate:-50%_-15%]" />
-              </span>
-              <span className="backdrop absolute inset-[1px] rounded-full bg-black transition-colors duration-200 group-hover:bg-neutral-900" />
-              <span className="h-full w-full blur-md absolute bottom-0 inset-x-0 bg-gradient-to-tr from-primary/20"></span>
-              <span className="z-10 py-0.5 text-sm text-neutral-100 flex items-center justify-center gap-1">
-                ✨ Manage links smarter
-                <ArrowRightIcon className="ml-1 size-3 transition-transform duration-300 ease-in-out group-hover:translate-x-0.5" />
-              </span>
+            <button className="py-10">
+              <StarWarsButton />
             </button>
             <h1 className="text-foreground text-center py-6 text-5xl font-medium tracking-normal text-balance sm:text-6xl md:text-7xl lg:text-8xl !leading-[1.15] w-full font-heading">
               Smart Links with{" "}
