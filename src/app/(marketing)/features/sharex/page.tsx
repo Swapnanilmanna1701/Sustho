f43651ab91305f6531177f256bba1797004ccdbf
Swapnanilmanna1701 <sm.22u10337@btech.nitdgp.ac.in>
2025-01-30 03:06:40 +0530

    marketing page error solved

--- src/app/(marketing)/features/sharex/page.tsx
+++ src/app/(marketing)/features/sharex/page.tsx
@@ -1,6 +1,6 @@
 import Hero from "@/components/sharex/Hero";
 import HowItWorks from "@/components/sharex/HowItWorks";
-import Navbar from "@/components/navigation/navbar"
+import Navbar from "@/components/navigation/navbar";
 
 export default function Home() {
   return (
